e458c16a0c478c0ac00117e35677e96e338ed2cb
mario <macndesign@gmail.com>
2013-04-03 17:45:34 -0300

    admin com mais opcoes

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,8 @@ from core.models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['autor', 'titulo', 'data_publicacao']
+    list_display = ('autor', 'titulo', 'data_publicacao')
+    list_filter = ('autor', 'data_publicacao')
+    search_fields = ['autor', 'titulo']
 
 admin.site.register(Post, PostAdmin)
