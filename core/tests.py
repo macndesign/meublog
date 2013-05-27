e33df93c96b48e3b4b5b661afdc2bdf0102baf5b
Mario Chaves <macndesign@gmail.com>
2013-05-27 12:01:25 -0300

    alguns comentarios e importando o utc do timezons do django

--- core/tests.py
+++ core/tests.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 from django.test import TestCase
-from .models import Post
 from django.contrib.auth.models import User
+from django.utils.timezone import utc
+from .models import Post
 from datetime import datetime
-import pytz
 
 
 class BlogTestCase(TestCase):
@@ -35,29 +35,40 @@ class BlogTestCase(TestCase):
 
     def test_postagem_tem_data_publicacao(self):
         self.assertIsInstance(self.postagem.data_publicacao, datetime)
-        self.assertEqual(self.postagem.data_publicacao.date(), datetime.now(tz=pytz.utc).date())
-        self.assertEqual(self.postagem.data_publicacao.time().hour, datetime.now(tz=pytz.utc).time().hour)
-        self.assertEqual(self.postagem.data_publicacao.time().minute, datetime.now(tz=pytz.utc).time().minute)
+        self.assertEqual(self.postagem.data_publicacao.date(), datetime.now(tz=utc).date())
+        self.assertEqual(self.postagem.data_publicacao.time().hour, datetime.now(tz=utc).time().hour)
+        self.assertEqual(self.postagem.data_publicacao.time().minute, datetime.now(tz=utc).time().minute)
 
     def test_postagem_tem_tags(self):
         tags_postagem = [tag.name for tag in self.postagem.tags.all()]
         tags_postagem_ativa = [tag.name for tag in self.postagem_ativa.tags.all()]
 
+        # Testando se a 1ª postagem tem 3 tags e a segunda tem 2 tags
         self.assertEqual(len(tags_postagem), 3)
         self.assertEqual(len(tags_postagem_ativa), 2)
 
+        # Testando especificamente se as tags estão ou não nas postagens
         for tag in ['primeira', 'postagem', 'teste']:
             self.assertIn(tag, tags_postagem)
+
+        self.assertNotIn('segunda', tags_postagem)
 
         for tag in ['segunda', 'postagem']:
             self.assertIn(tag, tags_postagem_ativa)
 
+        for tag in ['primeira', 'teste']:
+            self.assertNotIn(tag, tags_postagem_ativa)
+
+    # Testando se a postagem é criada por padrão como inativa
     def test_postagem_inativa_por_default(self):
         self.assertFalse(self.postagem.ativo)
 
+    # Testando se a página inicial está disponível e com o template correto
     def test_pagina_inicial(self):
         self.assertEqual(self.resp.status_code, 200)
         self.assertTemplateUsed(self.resp, 'core/post_list.html')
 
+    # Testando se aparece apenas a postagem que está ativa na página inicial
     def test_postagem_na_pagina_inicial(self):
         self.assertContains(self.resp, 'Segunda postagem')
+        self.assertNotContains(self.resp, 'Primeira postagem')
